12b08dc172210a9d49b72185c0a263ffe5f08cc6
amoyakd <yomauttad@gmail.com>
2017-07-25 14:52:25 +0530

    Update SQL Queries.R

--- SQL Queries.R
+++ SQL Queries.R
@@ -29,3 +29,28 @@ order by column limit 1
 # LEFT (OUTER) JOIN: Return all records from the left table, and the matched records from the right table
 # RIGHT (OUTER) JOIN: Return all records from the right table, and the matched records from the left table
 # FULL (OUTER) JOIN: Return all records when there is a match in either left or right table
+
+# Inner Join Syntax
+SELECT column_name(s)
+FROM table1
+INNER JOIN table2 ON table1.column_name = table2.column_name;
+
+# Inner join with 3 tables
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
+
+# Left join syntax. Left join is useful when we want to retain all columns in the LEFT table irrespective 
+# of matching values in right table. The result is NULL from the right side, if there is no match.
+# The below query gives all the customer names and their order ids
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+ORDER BY Customers.CustomerName;
+
+# The beow query gives the count of orders per customer using LEFT JOIN
+SELECT Customers.CustomerName, count(Orders.OrderID)
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerName;
